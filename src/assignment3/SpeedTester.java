cbc58be505ccbf06fe7a4c75b688fd30f99ab37e
croissantfriend <34118891+croissantfriend@users.noreply.github.com>
2020-12-02 18:08:04 -0700

    Update SpeedTester.java

--- src/assignment3/SpeedTester.java
+++ src/assignment3/SpeedTester.java
@@ -1,5 +1,6 @@
 package assignment3;
 
+import RuntimeTester.Visualizer;
 import RuntimeTester.benchmark;
 
 import java.io.File;
@@ -9,7 +10,9 @@ import java.util.Scanner;
 
 public class SpeedTester {
 
-    @benchmark(name = "shelter()", expectedEfficiency = "O(log(n))", category = "DogShelter")
+    public static final String categoryName = "DogShelter";
+
+    @benchmark(name = "shelter()", expectedEfficiency = "O(log(n))", category = categoryName)
     public static long testShelter(long input) throws FileNotFoundException {
         Random random = new Random();
         File file = new File("src/assignment3/first_names.all.txt");
@@ -18,18 +21,19 @@ public class SpeedTester {
         if (scanner.hasNextLine())
             name = scanner.nextLine();
         Dog d = new Dog("FirstDog", random.nextInt(), random.nextInt(), random.nextInt(), random.nextDouble());
-
         DogShelter shelter = new DogShelter(d);
         Dog toAdd = new Dog(name, random.nextInt(), random.nextInt(), random.nextInt(), random.nextDouble());
+        /* You can use TreePrinter here to show you the tree before the add - just be careful when
+           using it for very large trees! */
         long startTime = System.nanoTime();
-//        for (int i = 0; i < input; i++) {
         shelter.shelter(toAdd);
-//        }
         long endTime = System.nanoTime();
+        /* You can use TreePrinter here to show you the tree after to make sure you're doing this right - just be careful when
+           using it for very large trees! */
         return endTime - startTime;
     }
 
-    @benchmark(name = "shelter()", expectedEfficiency = "O(log(n))", category = "DogShelter")
+    @benchmark(name = "shelter()", expectedEfficiency = "O(log(n))", category = categoryName)
     public static long testAdopt(long input) throws FileNotFoundException {
         Random random = new Random();
         File file = new File("src/assignment3/first_names.all.txt");
@@ -43,9 +47,40 @@ public class SpeedTester {
         for (int i = 0; i < input; i++) {
             shelter.shelter(toAdopt);
         }
+        /* You can use TreePrinter here to show you the tree before the adoption - just be careful when
+           using it for very large trees! */
         long startTime = System.nanoTime();
         shelter.adopt();
         long endTime = System.nanoTime();
+        /* You can use TreePrinter here to show you the tree after to make sure you're doing this right - just be careful when
+           using it for very large trees! */
         return endTime - startTime;
     }
+
+    @benchmark(name = "findOldest()", expectedEfficiency = "O(log(n))", category = categoryName)
+    public static long testFindOldest(long input) {
+        Random random = new Random();
+        Dog d = new Dog("FirstDog", random.nextInt(), random.nextInt(), random.nextInt(), random.nextDouble());
+        DogShelter shelter = new DogShelter(d);
+        long startTime = System.nanoTime();
+        Dog o = shelter.findOldest();
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+
+    @benchmark(name = "findYoungest()", expectedEfficiency = "O(log(n))", category = categoryName)
+    public static long testFindYoungest(long input) {
+        Random random = new Random();
+        Dog d = new Dog("FirstDog", random.nextInt(), random.nextInt(), random.nextInt(), random.nextDouble());
+        DogShelter shelter = new DogShelter(d);
+        long startTime = System.nanoTime();
+        Dog y = shelter.findYoungest();
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+
+    public static void main(String[] args) {
+        Visualizer.launch(SpeedTester.class);
+    }
+
 }
