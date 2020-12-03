d26f9c8935a1999c01264d6bcd7460db2d28f965
croissantfriend <34118891+croissantfriend@users.noreply.github.com>
2020-12-02 20:42:44 -0700

    Update SpeedTester.java

--- src/assignment3/SpeedTester.java
+++ src/assignment3/SpeedTester.java
@@ -12,28 +12,7 @@ public class SpeedTester {
 
     public static final String categoryName = "DogShelter";
 
-    @benchmark(name = "shelter()", expectedEfficiency = "O(log(n))", category = categoryName)
-    public static long testShelter(long input) throws FileNotFoundException {
-        Random random = new Random();
-        File file = new File("src/assignment3/first_names.all.txt");
-        Scanner scanner = new Scanner(file);
-        String name = "None";
-        if (scanner.hasNextLine())
-            name = scanner.nextLine();
-        Dog d = new Dog("FirstDog", random.nextInt(), random.nextInt(), random.nextInt(), random.nextDouble());
-        DogShelter shelter = new DogShelter(d);
-        Dog toAdd = new Dog(name, random.nextInt(), random.nextInt(), random.nextInt(), random.nextDouble());
-        /* You can use TreePrinter here to show you the tree before the add - just be careful when
-           using it for very large trees! */
-        long startTime = System.nanoTime();
-        shelter.shelter(toAdd);
-        long endTime = System.nanoTime();
-        /* You can use TreePrinter here to show you the tree after to make sure you're doing this right - just be careful when
-           using it for very large trees! */
-        return endTime - startTime;
-    }
-
-    @benchmark(name = "shelter()", expectedEfficiency = "O(log(n))", category = categoryName)
+    @benchmark(name = "adopt()", expectedEfficiency = "O(log(n))", category = categoryName)
     public static long testAdopt(long input) throws FileNotFoundException {
         Random random = new Random();
         File file = new File("src/assignment3/first_names.all.txt");
